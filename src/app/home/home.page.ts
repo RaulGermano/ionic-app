7864bebf77e66c699e427cc6aa4ed04e9f550966
RaulGermano <raul.germano@icloud.com>
2019-09-14 17:57:12 -0300

    consulting a Pokemon api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss']
 })
 export class HomePage {
+	pokemonList: any = [];
 
-  constructor() {}
+	constructor(public http: HttpClient) {}
 
+	async GetPokemonList() {
+		await this.http
+			.get('https://pokeapi.co/api/v2/pokemon/')
+			.subscribe(data => {
+				const list = <any>data;
+
+				this.pokemonList = list.results;
+			});
+	}
 }
